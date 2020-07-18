e2a2134340d0cc83c4ac22b9e554b6a6a575a3de
damiel <daniel@users.noreply.github.com>
2020-07-18 18:12:54 +0800

    专题一第一章第一节

---
+++ src/com/crudsavior/zt1/d1z/d1j/Demo1.java
@@ -0,0 +1,11 @@
+package com.crudsavior.zt1.d1z.d1j;
+
+public class Demo1{
+    public static void main(String[] args){
+        int x = 500;
+        int y = 100;
+        int a = x / y;
+        int b = 50;
+        System.out.println(a + b);
+    }
+}
